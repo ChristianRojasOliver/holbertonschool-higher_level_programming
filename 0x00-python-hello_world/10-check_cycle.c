402e9f793e830258f83533f1b3f89e7ff96c06bb
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-09-08 00:52:08 +0000

    Check Cycle and important for the F Interview

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,13 +9,13 @@ int check_cycle(listint_t *list)
 	listint_t *fast;
 	listint_t *slow;
 
-	slow = list;
-	fast = list->next;
-
 	if (list == NULL)
 	{
 		return (0);
 	}
+	slow = list;
+	fast = list->next;
+
 	while (slow && fast && fast->next)
 	{
 		if (slow == fast)
