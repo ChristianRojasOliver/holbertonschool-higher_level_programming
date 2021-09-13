a79a3d1b168241eb89fc55576c8d3f2b991ebd2b
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-09-13 21:14:43 +0000

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    value = len(sys.argv) - 1
+
+    if value == 0:
+        print("0 arguments.")
+    elif value == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(value))
+
+    for i, arg in enumerate(sys.argv[1:], 1):
+        print("{}: {}".format(i, arg))
