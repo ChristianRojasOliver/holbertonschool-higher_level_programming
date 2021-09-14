c98ff7e3b64c4f1a009694de1b09cc43bca211ea
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-09-14 19:16:41 +0000

    Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
+    for r in range(len(matrix)):
+        for x in range(len(matrix[r])):
+            if (x < len(matrix[r]) - 1):
+                print("{:d}".format(matrix[r][x]), end=" ")
+            else:
+                print("{:d}".format(matrix[r][x]))
