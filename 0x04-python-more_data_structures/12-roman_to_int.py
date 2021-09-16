040aa764cac07aec2800ecdb9a3bfe5179ef4726
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-09-16 22:46:19 +0000

    roman numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,3 @@ def roman_to_int(roman_string):
   d = {'m': 1000, 'd': 500, 'c': 100, 'l': 50, 'x': 10, 'v': 5, 'i': 1}
   n = [d[i] for i in roman_string.lower() if i in d]
   return sum([i if i>=n[min(j+1, len(n)-1)] else -i for j,i in enumerate(n)])
-
-print(roman_to_int('X')) # 10
-print(roman_to_int('V')) # 5
-print(roman_to_int('IV')) # 4
-print(roman_to_int('XV')) # 15
