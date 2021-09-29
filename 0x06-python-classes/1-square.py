75aaeba2cd5cd3bca4078ee1204510f470f505de
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-09-29 02:32:52 +0000

    fixing comments

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Escribe una clase llamada square que define square'''
+'''Crear un private instance atribute'''
+'''luego inisiar'''
+
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
