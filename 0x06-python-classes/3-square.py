23bdcc52c397af5a7d4857973a5cfca4b80a9dcb
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-09-29 03:25:58 +0000

    size * size

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Module'''
+
+
+class Square:
+    '''comment'''
+    def __init__(self, size=0):
+        '''manipulando data'''
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        '''comment'''
+        return self.__size * self.__size
