8aad6f00637dce75bf0c22bddabdbc8858c5b535
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-10-19 19:19:11 +0000

    checking

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
     """Inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
