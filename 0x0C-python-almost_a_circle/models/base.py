ce54fc65277d4cd54fbb7e4f4ef5a7af17b1e5d7
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-10-19 02:59:51 +0000

    every task from 1 to 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Base Class"""
 
-
 class Base:
     """Function"""
     __nb_objects = 0
@@ -12,8 +11,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-obj = Base(1)
-cro = Base(2)
-print(obj.id)
-print(cro.id)
