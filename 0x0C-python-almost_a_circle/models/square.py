26d6ff857b296a3e0f2654d95d415b629ed226cc
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-10-19 19:20:59 +0000

    checking

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,6 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
@@ -30,7 +29,7 @@ class Square(Rectangle):
 
         return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
                                              self.id, self.x, self.y,
-                                             self.size)
+                                             self.width)
 
     def update(self, *args, **kwargs):
         if len(kwargs) != 0:
