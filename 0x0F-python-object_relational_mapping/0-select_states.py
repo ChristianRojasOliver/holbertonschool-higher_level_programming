75e456053000c8194b348589f18d66c9e08647ab
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-12-20 00:21:27 +0000

    add

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,11 @@
 Module returns a list of all the states from a database
 '''
 
-if __name__ == '__main__':
 
 import MySQLdb
 from sys import argv
+
+if __name__ == '__main__':
 
     connector = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                                 passwd=argv[2], db=argv[3], charset='utf8')
