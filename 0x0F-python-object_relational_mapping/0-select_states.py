726ceff10010579acd78f5bfd13bcd52a3dffead
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-12-20 00:25:51 +0000

    add

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-'''
-Module returns a list of all the states from a database
-'''
+""" Made by Christian Rojas to Holberton School 2021 """
 
+if __name__ == "__main__":
 
-import MySQLdb
-from sys import argv
-
-if __name__ == '__main__':
-
-    connector = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                                passwd=argv[2], db=argv[3], charset='utf8')
-    curs = connector.cursor()
-    curs.execute('SELECT * FROM states ORDER BY states.id ASC')
-    table = curs.fetchall()
-    for row in table:
-        print(row)
-    curs.close()
-    connector.close()
+    from sys import argv
+    import MySQLdb
+    MY_H = "localhost"
+    MY_U = argv[1]
+    MY_P = argv[2]
+    MY_D = argv[3]
+    nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nuevaconexion.cursor()
+    consulta.execute("SELECT * FROM states")
+    resultado = consulta.fetchall()
+    for fila in resultado:
+        print(fila)
+    nuevaconexion.close()
