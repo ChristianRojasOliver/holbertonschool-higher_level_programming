808f893e409930c65277de750668b09690114996
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-12-20 01:42:14 +0000

    add

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
     consulta = nuevaconexion.cursor()
     consulta.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR\
-                     ', ') FROM cities INNER JOIN states ON states.name\
-                      = %s WHERE cities.state_id = states.id ORDER\
-                       BY cities.id ASC;", (argument,))
+            ', ') FROM cities INNER JOIN states ON states.name\
+            = %s WHERE cities.state_id = states.id ORDER\
+            BY cities.id ASC;", (argument,))
     resultado = consulta.fetchall()
     for fila in resultado:
-            proceso = str(fila).replace(",)", "")
-            proceso = proceso.replace("(", "")
-            proceso = proceso.replace("'", "")
+        proceso = str(fila).replace(",)", "")
+        proceso = proceso.replace("(", "")
+        proceso = proceso.replace("'", "")
             if (proceso != 'None'):
                 print(proceso)
             else:
