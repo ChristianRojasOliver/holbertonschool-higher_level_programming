ca6a29038bddf2fd723be7c7a77d53334879fc88
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-12-22 01:40:03 +0000

    add

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const viejo = require('./5-square');
+
+// Made by Christian Rojas to Holberton School 2021
+
+class Square extends viejo {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
