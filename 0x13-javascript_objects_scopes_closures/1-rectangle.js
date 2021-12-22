ff5a61c7a73fc1f4340e44aa3c9ed32112bd6464
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-12-22 01:29:30 +0000

    constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Made by Christian Rojas to Holberton School 2021
+class Rectangle {
+  constructor (h, w) {
+    this.width = h;
+    this.height = w;
+  }
+}
+module.exports = Rectangle;
