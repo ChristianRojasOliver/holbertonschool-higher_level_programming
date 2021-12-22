48d00a37ba98ff9a804d04eb30053d4d2bdba4fa
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-12-22 01:41:48 +0000

    add

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Made by Christian Rojas to Holberton School 2021
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
