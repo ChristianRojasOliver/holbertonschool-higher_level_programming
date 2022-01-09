c770d4a8e0f1606d066574e0aee915818ad68e11
ChristianRojasOliver <christian.rojas201@gmail.com>
2022-01-09 18:25:36 +0000

    call not fetch, Body response very important!

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Made by Christian from holbertonschool 2022 """
+
+
 import urllib.request as llamada
 
 if __name__ == "__main__":
-    with llamada.urlopen('https://intranet.hbtn.io/status') as res:
-        datos = res.read()
+    with llamada.urlopen('https://intranet.hbtn.io/status') as response:
+        datos = response.read()
         print('Body response:')
         print("\t- type: {}".format(type(datos)))
         print("\t- content: {}".format(datos))
