1b85c7e90ed6e6fdf7a22d74d78e98e577b93454
ChristianRojasOliver <christian.rojas201@gmail.com>
2022-01-09 23:08:43 +0000

    interview question

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" funcion para encontrar la altura en una lista of unsorted integers """
+
+
+def find_peak(int_list):
+    """ Encontrando la Altura de la lista """
+    """ Board cases """
+
+    if len(int_list) == 0:
+        return None
+    if len(int_list) == 1:
+        return int_list[0]
+    if len(int_list) == 2:
+        return max(int_list)
+
+    mid_point = int(len(int_list)/2)
+    middle = int_list[mid_point]
+
+    if middle > int_list[mid_point - 1] and middle > int_list[mid_point + 1]:
+        return middle
+    elif middle < int_list[mid_point - 1]:
+        return find_peak(int_list[:mid_point])
+    else:
+        return find_peak(int_list[mid_point + 1:])
