5ee5cf10552d465dca6db7d4abb6f17494439170
ChristianRojasOliver <christian.rojas201@gmail.com>
2022-01-09 21:26:23 +0000

    fixing

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Made by Christian """
+import requests
+import sys
+
+if __name__ == "__main__":
+    userxd = sys.argv[1]
+    passxd = sys.argv[2]
+    link = "https://api.github.com/user"
+    res = requests.get(link, auth=(userxd, passxd))
+    print(res.json().get('id'))
