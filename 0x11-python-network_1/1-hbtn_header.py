6bbadffe760ea187b4552ae98930290be9fcfcdb
ChristianRojasOliver <christian.rojas201@gmail.com>
2022-01-09 17:18:51 +0000

    response

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Made by Christian """
+import urllib.request as llamada
+from sys import argv
+
+if __name__ == "__main__":
+    datos = llamada.Request(argv[1])
+    with llamada.urlopen(datos) as res:
+        print(res.headers.get('X-Request-Id'))
