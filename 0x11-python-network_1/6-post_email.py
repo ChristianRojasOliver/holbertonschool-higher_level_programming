d5df8c26f96139cda0639429fddb2e5852bbb1d5
ChristianRojasOliver <christian.rojas201@gmail.com>
2022-01-09 20:59:58 +0000

    post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Made by Christian """
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    res = requests.post(argv[1], data={'email': argv[2]})
+    print(res.text)
