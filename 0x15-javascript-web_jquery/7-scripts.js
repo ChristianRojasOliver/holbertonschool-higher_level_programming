5a378b8b4bf8de57291bb1d8a338f0f87ddd7d63
ChristianRojasOliver <christian.rojas201@gmail.com>
2022-02-09 18:26:43 +0000

    add task 7

---
+++ 0x15-javascript-web_jquery/7-scripts.js
@@ -0,0 +1,7 @@
+/* Made by Christian */
+$(function () {
+    const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+    $.get(url, function (data, textStatus) {
+      $('#character').text(data.name);
+    });
+  });
